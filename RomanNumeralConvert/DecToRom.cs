242afc4e615c5948a2060e37f453381b912b3cce
danFbach <quiktekrepair@gmail.com>
2016-03-15 17:58:25 -0600

    cleaned up code, easier to read

--- RomanNumeralConvert/DecToRom.cs
+++ RomanNumeralConvert/DecToRom.cs
@@ -15,58 +15,25 @@ namespace RomanNumeralConvert
                     + "you would like converted to Roman Numeral." + LineBreak);
                 number = int.Parse(Console.ReadLine());
                 if (number >= 1 || number <= 10000)
-                {while (number >= 10000)
-                    {   Roman += "x";
-                        number -= 10000;
-                    }while (number >= 9000)
-                    {   Roman += "Mx";
-                        number -= 9000;
-                    }while (number >= 5000)
-                    {   Roman += "v";
-                        number -= 5000;
-                    }while (number >= 4000)
-                    {   Roman += "Mv";
-                        number -= 4000;
-                    }while (number >= 1000)
-                    {   Roman += "M";
-                        number -= 1000;
-                    }while (number >= 900)
-                    {   Roman += "CM";
-                        number -= 900;
-                    }while (number >= 500)
-                    {   Roman += "D";
-                        number -= 500;
-                    }while (number >= 400)
-                    {   Roman += "CD";
-                        number -= 400;
-                    }while (number >= 100)
-                    {   Roman += "C";
-                        number -= 100;
-                    }while (number >= 90)
-                    {   Roman += "XC";
-                        number -= 90;
-                    }while (number >= 50)
-                    {   Roman += "L";
-                        number -= 50;
-                    }while (number >= 40)
-                    {   Roman += "XL";
-                        number -= 40;
-                    }while (number >= 10)
-                    {   Roman += "X";
-                        number -= 10;
-                    }while (number >= 9)
-                    {   Roman += "IX";
-                        number -= 9;
-                    }while (number >= 5)
-                    {   Roman += "V";
-                        number -= 5;
-                    }while (number >= 4)
-                    {   Roman += "IV";
-                        number -= 4;
-                    }while (number >= 1)
-                    {   Roman += "I";
-                        number -= 1;
-                    }   Console.WriteLine(Roman + LineBreak);
+                {
+                while (number >= 10000){Roman += "x";number -= 10000;}
+                while (number >= 9000){Roman += "Mx";number -= 9000;}
+                while (number >= 5000){Roman += "v";number -= 5000;}
+                while (number >= 4000){Roman += "Mv";number -= 4000;}
+                while (number >= 1000){Roman += "M";number -= 1000;}
+                while (number >= 900){Roman += "CM";number -= 900;}
+                while (number >= 500){Roman += "D";number -= 500;}
+                while (number >= 400){Roman += "CD";number -= 400;}
+                while (number >= 100){Roman += "C";number -= 100;}
+                while (number >= 90){Roman += "XC";number -= 90;}
+                while (number >= 50){Roman += "L";number -= 50;}
+                while (number >= 40){Roman += "XL";number -= 40;}
+                while (number >= 10){Roman += "X";number -= 10;}
+                while (number >= 9){Roman += "IX";number -= 9;}
+                while (number >= 5){Roman += "V";number -= 5;}
+                while (number >= 4){Roman += "IV";number -= 4;}
+                while (number >= 1){Roman += "I";number -= 1;}
+                Console.WriteLine(Roman + LineBreak);
             }
         }
     }
--- RomanNumeralConvert/RomToDec.cs
+++ RomanNumeralConvert/RomToDec.cs
@@ -14,58 +14,24 @@ namespace RomanNumeralConvert
             Console.Write("Please enter the Numeral from I to x(10,000) that" + LineBreak
                 + "you would like converted to Roman Numeral." + LineBreak);
             Roman = Console.ReadLine();
-            while (Roman.StartsWith("x"))
-            {   number += 10000;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("Mx"))
-            {   number += 9000;
-                Roman = Roman.Remove(0, 1);
-            } while (Roman.StartsWith("v"))
-            {   number += 5000;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("Mv"))
-            {   number += 4000;
-                Roman = Roman.Remove(0, 1);
-            } while (Roman.StartsWith("M"))
-            {   number += 1000;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("CM"))
-            {	number += 900;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("D"))
-            {	number += 500;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("CD"))
-            {	number += 400;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("C"))
-            {	number += 100;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("XC"))
-            {	number += 90;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("L"))
-            {	number += 50;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("XL"))
-            {	number += 40;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("X"))
-            {	number += 10;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("IX"))
-            {	number += 9;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("V"))
-            {	number += 5;
-                Roman = Roman.Remove(0, 1);
-            }while (Roman.StartsWith("IV"))
-            {	number += 4;
-                Roman = Roman.Remove(0, 2);
-            }while (Roman.StartsWith("I"))
-            {	number += 1;
-                Roman = Roman.Remove(0, 1);
-            }	Console.WriteLine(number + LineBreak);
+            while (Roman.StartsWith("x")){number += 10000;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("Mx")){number += 9000;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("v")){number += 5000;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("Mv")){number += 4000;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("M")){number += 1000;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("CM")){number += 900;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("D")){number += 500;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("CD")){number += 400;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("C")){number += 100;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("XC")){number += 90;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("L")){number += 50;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("XL")){number += 40;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("X")){number += 10;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("IX")){number += 9;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("V")){number += 5;Roman = Roman.Remove(0, 1);}
+            while (Roman.StartsWith("IV")){number += 4;Roman = Roman.Remove(0, 2);}
+            while (Roman.StartsWith("I")){number += 1;Roman = Roman.Remove(0, 1);}
+            Console.WriteLine(number + LineBreak);
 	    }
     }
 }
