c901187502e02c5d5ae0eb5ce3c4b6fa7b67868f
danFbach <quiktekrepair@gmail.com>
2016-03-16 14:16:17 -0600

    cleaning things up, making the program running smoothly

--- RomanNumeralConvert/RomToDec.cs
+++ RomanNumeralConvert/RomToDec.cs
@@ -8,30 +8,34 @@ namespace RomanNumeralConvert
 {   class RomToDec
     {   String LineBreak = Environment.NewLine;
         String Roman = "";
+        String RomanTemp;
         int number = 0;
         public void ToDecimal()
-        {   number = 0;
+        {
+            number = 0;
             Console.Write("Please enter the Numeral from I to x(10,000) that" + LineBreak
-                + "you would like converted to Roman Numeral." + LineBreak);
+                + "you would like converted to Roma1n Numeral." + LineBreak + LineBreak);
             Roman = Console.ReadLine();
-            while (Roman.StartsWith("x")){number += 10000;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("Mx")){number += 9000;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("v")){number += 5000;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("Mv")){number += 4000;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("M")){number += 1000;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("CM")){number += 900;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("D")){number += 500;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("CD")){number += 400;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("C")){number += 100;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("XC")){number += 90;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("L")){number += 50;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("XL")){number += 40;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("X")){number += 10;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("IX")){number += 9;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("V")){number += 5;Roman = Roman.Remove(0, 1);}
-            while (Roman.StartsWith("IV")){number += 4;Roman = Roman.Remove(0, 2);}
-            while (Roman.StartsWith("I")){number += 1;Roman = Roman.Remove(0, 1);}
-            Console.WriteLine(number + LineBreak);
+            RomanTemp = Roman;
+            while (RomanTemp.StartsWith("x"))       {number += 10000;   RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("Mx"))      {number += 9000;    RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("v"))       {number += 5000;    RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("Mv"))      {number += 4000;    RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("M"))       {number += 1000;    RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("CM"))      {number += 900;     RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("D"))       {number += 500;     RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("CD"))      {number += 400;     RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("C"))       {number += 100;     RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("XC"))      {number += 90;      RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("L"))       {number += 50;      RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("XL"))      {number += 40;      RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("X"))       {number += 10;      RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("IX"))      {number += 9;       RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("V"))       {number += 5;       RomanTemp = RomanTemp.Remove(0, 1);}
+            while (RomanTemp.StartsWith("IV"))      {number += 4;       RomanTemp = RomanTemp.Remove(0, 2);}
+            while (RomanTemp.StartsWith("I"))       {number += 1;       RomanTemp = RomanTemp.Remove(0, 1);}
+
+            Console.WriteLine(LineBreak + "You entered " + Roman + " which equals " + number + " in decimal base10." + LineBreak);
 	    }
     }
 }
